7be990270597132a92f41d46e6d371c7a7a773bd
cantus145 <cantus145@hotmail.com>
2020-04-05 21:13:43 +0800

    update

--- invoice_tool.py
+++ invoice_tool.py
@@ -1,6 +1,12 @@
-# 108 3-4
-title = "\n108年03-04月"
-allinvos = [ "602", "299", "270", "506", "409" ]
+
+
+##########################################################
+
+# 106 11 - 12
+title = "\n 106年 11-12 月"
+allinvos = [ "343","249","891","491","437"]
+
+##########################################################
 
 
 # 判斷3位數
@@ -10,7 +16,7 @@ def isValidInput(str):
 def printGoal(invo):
     idx = 5
     while idx > 0:
-         result = "中三碼 中三碼 中三碼 中三碼!!!!!!: " + invo
+         result = "中三碼 中三碼 中三碼 !!!!!: " + invo
          if idx == 5:
              print("\n")     
          print(result)        
@@ -18,7 +24,7 @@ def printGoal(invo):
              print("\n")
          idx-=1          
 
-start = True
+start = True 
 while (start):
     invo = input(title + "\n輸入後三碼:") .strip()      
 
